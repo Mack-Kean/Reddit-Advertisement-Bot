1b4c7e0877a41c362f83dc764655c94849315def
Mackenzie Kean <mackenziewkean@gmail.com>
2022-02-27 18:44:10 -0500

    added exception handling with error message if program cannot load the config file

--- bot.py
+++ bot.py
@@ -6,9 +6,14 @@ import json
 CLIENT_ID = 'pm25UeetuPduMhmHDgfocQ'
 SECRET_KEY = '-kHk4dAxWCAkfrbpM1Mz8EZ1qlZRBw'
 
-#open json confif file
-f = open('config.json')
-data = json.load(f)
+#open json confif file if possible
+try:
+    f = open('config.json') #file opened for reading
+    data = json.load(f)
+    f.close()
+except:
+    print('Error opening config file.  Ensure config.json is present in same directory as bot.py')
+    exit()
 
 reddit = praw.Reddit(
     client_id=data['client_id'],
@@ -38,7 +43,7 @@ if (data['image_filename'] != ""):
     selftext = '{image1}'
     media = {"image1": image}
 
-#this loop adds all platforms from config file
+#this loop adds all platforms from config file and uses markdown to make them look nice
 selftext += neatSummaryText + '\n\n# Find us here: \n\n'
 for platform in data['platforms']:
     selftext += '### [' + platform['name'] + '](' + platform['link'] + ')\n\n'
